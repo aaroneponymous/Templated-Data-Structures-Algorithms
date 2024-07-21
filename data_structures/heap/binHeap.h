a8e6c11878a2bee76adbb6466bb84d9b3129082c
aaroneponymous <apaul9@u.rochester.edu>
2024-07-21 19:51:19 -0400

    Comments + Percolate Down

--- data_structures/heap/binHeap.h
+++ data_structures/heap/binHeap.h
@@ -7,7 +7,7 @@ public:
     explicit BinaryHeap(const vector<Comparable> &items);
 
     bool isEmpty() const;
-    const Comparable& findMin() const;
+    const Comparable &findMin() const;
 
     /**
      * Insert item x, allowing duplicates
@@ -40,12 +40,33 @@ public:
      * Remove the minimum item
      * Throws UnderflowException if empty
      */
-    
-    void deleteMin() 
+
+    void deleteMin()
     {
-        if (isEmpty()) throw UnderflowException{ };
+        if (isEmpty())
+            throw UnderflowException{};
+
+        /** Move the minimum element (root of the heap) to minItem.
+         * array[1] contains the minimum element of the heap, as the binary heap property ensures the
+         * smallest element is always at the root. std::move(array[1]) casts array[1] to an rvalue reference,
+         * allowing for the move assignment operator to be used. This means the value of array[1] is moved to
+         * minItem, and array[1] is left in a valid but unspecified state.
+         */
+
+        Comparable minItem = std::move(array[1]);
+
+        /** Move the last element in the heap to the root position.
+         * current_size_ is the index of the last element in the heap.
+         * array[current_size_--] accesses the last element in the heap and then decrements current_size_ by 1,
+         * effectively removing the last element from the heap.
+         * std::move(array[current_size_--]) casts the last element to an rvalue reference,
+         * allowing for the move assignment operator to be used.
+         * array[1] = std::move(array[current_size_--]) moves the last element to the root position (index 1).
+         */
 
         array[1] = std::move(array[current_size_--]);
+
+        // Percolate down the element at the root to restore the heap property.
         percolateDown(1);
     }
 
@@ -53,7 +74,15 @@ public:
      * Remove the minimum item and place it in minItem
      * Throws UnderflowException if empty
      */
-    void deleteMin(Comparable &minItem);
+    void deleteMin(Comparable &minItem)
+    {
+        if (isEmpty())
+            throw UnderflowException{};
+
+        minItem = std::move(array[1]);
+        array[1] = std::move(array[current_size_--]);
+        percolateDown(1);
+    }
     void makeEmpty();
 
 private:
@@ -62,5 +91,21 @@ private:
 
     void buildHeap();
     // Hole:
-    void percolateDown(int hole);
+    void percolateDown(int hole)
+    {
+        int child;
+        Comparable tmp = std::move(array[hole]);
+
+        for ( ; hole * 2 <= current_size_; hole = child)
+        {
+            child = hole * 2;
+            if (child != current_size_ && array[child + 1] < array[child]) ++child;
+            if (array[child] < tmp) 
+            {
+                array[hole] = std::move(array[child]);
+            }
+            else break;
+        }
+        array[hole] = std::move(tmp);
+    }
 };
